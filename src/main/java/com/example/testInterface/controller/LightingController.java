0024f367eba6f84983f8509213edc0bc0433a116
ProdamGaraj <prodgaraj@yandex.ru>
2023-03-14 21:16:19 +0300

    sosi

--- src/main/java/com/example/testInterface/controller/LightingController.java
+++ src/main/java/com/example/testInterface/controller/LightingController.java
@@ -22,34 +22,36 @@ public class LightingController {
     }
 
     @GetMapping("")
-    public String test(Model model){
-        model.addAttribute("list",lightingService.getList());
+    public String test(Model model) {
+        model.addAttribute("list", lightingService.getList());
         return "mainPage";
     }
-    @GetMapping("{id}")
-    public String getLighting(@PathVariable("id")String id, Model model){
-        List<Lighting> arr = new ArrayList<>();
-        arr.add(lightingService.getLightingID(id));
-        model.addAttribute("lighting",arr);
-        return "editLighting";
-    }
+
+
+
     @PostMapping("/create")
-    public String createNewLighting(){
+    public String createNewLighting() {
         lightingService.saveNewLighting();
         System.out.println("Create new object");
         return "redirect:/";
     }
-    @PostMapping("/edit/{id}")
-    public String editLighting(@PathVariable("id")String id,
-                               @RequestParam(value = "collor_red",required = false)String collor_red,
-                               @RequestParam(value = "collor_blue",required = false)String collor_blue,
-                               @RequestParam(value = "collor_green",required = false)String collor_green,
-                               @RequestParam(value = "power_Wat",required = false)String power_Wat,
-                               @RequestParam(value = "lux",required = false)String lux ,
-                               @RequestParam(value = "uptime_days",required = false)String uptime_days ,
-                               @RequestParam(value = "status",required = false) String status)
+    @GetMapping("/edit/{id}")
+    public String getLighting(@PathVariable("id") String id, Model model) {
+        List<Lighting> arr = new ArrayList<>();
+        arr.add(lightingService.getLightingID(id));
+        model.addAttribute("lighting", arr);
+        return "editLighting";
+    }
 
-    {
+    @PostMapping("/edit/{id}")
+    public String editLighting(@PathVariable("id") String id,
+                               @RequestParam(value = "collor_red", required = false) String collor_red,
+                               @RequestParam(value = "collor_blue", required = false) String collor_blue,
+                               @RequestParam(value = "collor_green", required = false) String collor_green,
+                               @RequestParam(value = "power_Wat", required = false) String power_Wat,
+                               @RequestParam(value = "lux", required = false) String lux,
+                               @RequestParam(value = "uptime_days", required = false) String uptime_days,
+                               @RequestParam(value = "status", required = false) String status) {
         Lighting lighting = new Lighting(
                 Long.parseLong(id),
                 Integer.parseInt(collor_red),
@@ -59,31 +61,32 @@ public class LightingController {
                 Integer.parseInt(lux),
                 Integer.parseInt(uptime_days),
                 Boolean.getBoolean(status));
-        if (lighting!=null){
-            lightingService.update(lighting);
-            System.out.println("Save object controller");
-            return "redirect:/";
-        }
-        return "redirect:/error";
+
+        lightingService.update(lighting);
+        System.out.println("Save object controller");
+        return "redirect:/";
+
     }
-    @GetMapping("ERROR")
-    public String error(){
+
+    @GetMapping("error")
+    public String error() {
         return "test";
     }
 
     @GetMapping("/delete/{id}")
-    public String delete(@PathVariable("id")String id){
+    public String delete(@PathVariable("id") String id) {
         lightingService.deleteLighting(id);
         return "redirect:/";
     }
+
     @GetMapping("/info/get")
-    public String get4Buisnes(Model model){
-        model.addAttribute("list",lightingService.getList());
+    public String get4Buisnes(Model model) {
+        model.addAttribute("list", lightingService.getList());
         return "list4Buisnes";
     }
 
     @GetMapping("/info/switchLight/{id}")
-    public String switchLight(@PathVariable("id")String id){
+    public String switchLight(@PathVariable("id") String id) {
         lightingService.switchLight(id);
         return "redirect:/info/get";
     }
--- src/main/java/com/example/testInterface/entity/Lighting.java
+++ src/main/java/com/example/testInterface/entity/Lighting.java
@@ -20,14 +20,14 @@ public class Lighting {
     }
 
     public Lighting() {
-        //this.id=Long.parseLong("-1");
-        //this.collor_red=0;
-        //this.collor_blue=0;
-        //this.collor_green=0;
-        //this.power_Wat=0;
-        //this.lux=0;
-        //this.uptime_days=0;
-        //this.status=false;
+        this.id=Long.parseLong("-1");
+        this.collor_red=0;
+        this.collor_blue=0;
+        this.collor_green=0;
+        this.power_Wat=0;
+        this.lux=0;
+        this.uptime_days=0;
+        this.status=false;
     }
 
     public Lighting(long id, int collor_red, int collor_blue, int collor_green, double power_Wat, int lux, int uptime_days, boolean status) {
